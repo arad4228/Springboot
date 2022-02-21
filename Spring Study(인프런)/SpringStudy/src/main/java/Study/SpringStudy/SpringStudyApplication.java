d2d60c7c9e88478c5c1b7fde96de9e62d2436b66
arad4228 <arad4228@gmail.com>
2022-02-21 14:57:08 +0900

    프로젝트 생성강의 완료

--- Spring Study(인프런)/SpringStudy/src/main/java/Study/SpringStudy/SpringStudyApplication.java
+++ Spring Study(인프런)/SpringStudy/src/main/java/Study/SpringStudy/SpringStudyApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringStudyApplication {
-
+							// Spring Boot의 내장 TomCat실행.
 	public static void main(String[] args) {
 		SpringApplication.run(SpringStudyApplication.class, args);
 	}
