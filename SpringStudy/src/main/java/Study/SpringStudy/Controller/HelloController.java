7459781df6fec35156cb54485663dd346fcf1e3d
arad4228 <arad4228@gmail.com>
2022-02-22 11:25:55 +0900

    MVC, template

--- SpringStudy/src/main/java/Study/SpringStudy/Controller/HelloController.java
+++ SpringStudy/src/main/java/Study/SpringStudy/Controller/HelloController.java
@@ -1,8 +1,10 @@
 package Study.SpringStudy.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.thymeleaf.model.IModel;
 
 @Controller
@@ -13,5 +15,10 @@ public class HelloController {
     {
         model.addAttribute("data","hello!!");
         return "hello";
+    }
+    @GetMapping("hello-mvc")
+    public  String hellomvc(@RequestParam(value = "name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
