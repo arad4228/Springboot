122531b2acf3d1a769e34dd4bef02d6ef083c4ea
arad4228 <arad4228@gmail.com>
2022-02-23 21:39:49 +0900

    회원 웹의 홈페이지, 등록, 조회 페이지 및 기능

--- SpringExample/src/main/java/exampleBack/arad4228/controller/MemberController.java
+++ SpringExample/src/main/java/exampleBack/arad4228/controller/MemberController.java
@@ -1,8 +1,14 @@
 package exampleBack.arad4228.controller;
 
+import exampleBack.arad4228.domain.Member;
 import exampleBack.arad4228.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -15,4 +21,26 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+    //조회할 때 사용.
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    // 데이터를 form에 넣어 전달할 때 사용.
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+         List<Member> members = memberService.findMembers();
+         model.addAttribute("members", members);
+         return "members/memberList";
+    }
 }
