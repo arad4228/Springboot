e328feae56e84ca6766443b9ea494e15cd05f8c5
arad4228 <arad4228@gmail.com>
2022-02-24 13:32:59 +0900

    순수 JDBC연결(H2)

--- SpringExample/src/main/java/exampleBack/arad4228/service/springconfig.java
+++ SpringExample/src/main/java/exampleBack/arad4228/service/springconfig.java
@@ -1,13 +1,22 @@
 package exampleBack.arad4228.service;
 
-import exampleBack.arad4228.domain.Member;
+import exampleBack.arad4228.repository.JdbcMemberRepository;
 import exampleBack.arad4228.repository.MemberRepository;
-import exampleBack.arad4228.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class springconfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public springconfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,7 +25,8 @@ public class springconfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-        }
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
+    }
 }
 
