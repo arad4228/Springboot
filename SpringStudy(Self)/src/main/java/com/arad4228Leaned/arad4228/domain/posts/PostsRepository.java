b3b43f096ad636b9d9ad7b3ff49138a962ea49e1
arad4228 <arad4228@gmail.com>
2022-03-09 15:22:59 +0900

    posts관련 단위 테스트 완료

--- SpringStudy(Self)/src/main/java/com/arad4228Leaned/arad4228/domain/posts/PostsRepository.java
+++ SpringStudy(Self)/src/main/java/com/arad4228Leaned/arad4228/domain/posts/PostsRepository.java
@@ -2,5 +2,5 @@ package com.arad4228Leaned.arad4228.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Posts, Long> {
+public interface PostsRepository extends JpaRepository<Posts, Long> {
 }
--- SpringStudy(Self)/src/test/java/com/arad4228Leaned/arad4228/domain/posts/PostsRepositoryTest.java
+++ SpringStudy(Self)/src/test/java/com/arad4228Leaned/arad4228/domain/posts/PostsRepositoryTest.java
@@ -1,5 +1,43 @@
 package com.arad4228Leaned.arad4228.domain.posts;
 
+import org.aspectj.lang.annotation.After;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
 public class PostsRepositoryTest {
-    
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        // given
+        String title = "테스트게시물";
+        String context = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .context(context)
+                .author("arad4228")
+                .build());
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContext()).isEqualTo(context);
+    }
 }
